665062e668811bd69b530837d30412b27d16382d
mrgiesz <mrgiesz@gmail.com>
2022-01-04 09:44:13 +0100

    Refractored code

--- Fortigate/main.py
+++ Fortigate/main.py
@@ -8,6 +8,7 @@ import urllib3
 # disable ssl warnings
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
+
 def get_data_from_fortigate(name, ip, port, api_key):
     api_url = 'https://' + str(ip) + ':' + str(
         port) + '/api/v2/monitor/system/config/backup/?scope=global&access_token=' + api_key
